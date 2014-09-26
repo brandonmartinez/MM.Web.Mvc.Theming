513513f79b66c4017726b2935ad189e16c2a14cf
Brandon Martinez <me@brandonmartinez.com>
2014-09-26 16:35:23 -0400

    Initial commit of projects

---
+++ MvcThemingExample/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace MvcThemingExample
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
